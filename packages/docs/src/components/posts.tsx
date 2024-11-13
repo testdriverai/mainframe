00726e9ba13c465bd874f0774e72ef197b96d03b
Tarun Kumar S <srfashions.tarun@gmail.com>
2024-11-13 15:08:15 +0530

    add mdx support

--- packages/docs/src/components/posts.tsx
+++ packages/docs/src/components/posts.tsx
@@ -16,7 +16,7 @@ export const sections = [
   return {
     name: section.name,
     pages: section.pages.map((slug) => {
-      const post = allPosts.find((post) => post.fileName === slug);
+      const post = allPosts.find((post) => post.fileName === `${slug}.md` || post.fileName === `${slug}.mdx`);
       if (!post) {
         throw new Error(`Missing post: ${slug}`);
       }
